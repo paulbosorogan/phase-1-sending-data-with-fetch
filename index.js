caae084387a8623c793496639a5d3ea731f4cb01
Paul Bosorogan <bosorogan.paul@gmail.com>
2022-10-16 16:45:12 -0400

    commit tree 187802a76fa10315081da17368f813ceb8087507

--- index.js
+++ index.js
@@ -1,1 +1,17 @@
 // Add your code here
+function submitData(userName, userEmail){
+ return fetch('http://localhost:3000/users', {
+    method: 'POST',
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+    },
+    body: JSON.stringify ({
+        "name" : `${userName}`,
+        "email" : `${userEmail}`,
+    }),
+ })
+ .then(res => {return res.json()})
+ .then(obj => { document.querySelector('body').innerHTML= obj.id})
+ .catch(error => { alert("Error!"); document.querySelector('body').innerHTML = error.message})
+ }
